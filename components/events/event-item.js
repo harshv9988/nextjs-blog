bdc685c8a03db75039c29c4087850e65b479ecd9
harshv9988 <harshv9988@gmail.com>
2022-10-26 14:40:57 +0530

    ui with mock data

---
+++ components/events/event-item.js
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import Image from "next/image";
+
+import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+
+export default function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanRedableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+  const expoloreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time> {humanRedableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            {formattedAddress}
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={expoloreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
+}
